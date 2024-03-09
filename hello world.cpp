dce65dfdee07261fcf0cb37a6a1aae5de456a797
CharlieSun <charliesunexl@gmail.com>
2024-03-09 10:54:34 +0800

    hello world

--- hello world.cpp
+++ hello world.cpp
@@ -1,8 +1,8 @@
-#hello world.cpp
 #include <iostream>
+
 int main(){
-    std::cout <<"hello world, this is charlie.\n"
+    std::cout <<"hello world, this is charlie.\n";
     std::cout <<"miss me?";
-    return 0
+    return 0;
 
 }
